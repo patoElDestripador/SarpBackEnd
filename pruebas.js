7d7389f9818e27d1ddaa722b419f48e750101873
pepenauta17 <patoasesino69@gamil.com>
2024-01-19 15:04:17 -0500

    pruebas

---
+++ pruebas.js
@@ -0,0 +1,10 @@
+let arrayAnimales = ["gato","pez","perro","lagarto","zanahoria","conejo"]
+
+//el conejo se ha traido su comido al nido de los animales debemos de eliminar la comida de alli
+
+
+for(let index = 0;index < arrayAnimales.length; index++){
+  console.log("hola")
+  if(arrayAnimales.length){
+  }
+}
